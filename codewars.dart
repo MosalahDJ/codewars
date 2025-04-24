0f07a27ba2f11d22b060dbe2416e8f802222d6ea
mohamedsalah <djehelmohamedsalah@gmail.com>
2025-04-24 11:25:16 +0100

    problem fixed to next problem

--- codewars.dart
+++ codewars.dart
@@ -1,40 +1,16 @@
 void main() {
   String testText = "hello  hello";
-  String reverseWords(String text) {
-    List words = [];
-    List reverssedwords = [];
-    String word = "";
-    List finalreverse = [];
-    words.addAll(text.split(" "));
-    words.removeWhere((e) => e == "");
-    for (int i = 0; i < words.length; i++) {
-      reverssedwords = words[i].toString().split("");
-      finalreverse.add(reverssedwords.reversed.join());
-      reverssedwords.clear();
+String reverseWords(String text) {
+  List<String> parts = text.split(' ');
+    for (int i = 0; i < parts.length; i++) {
+      if (parts[i].isNotEmpty) {
+        parts[i] = parts[i].split('').reversed.join();
+      }
     }
-    for (int i = 0; i < finalreverse.length; i++) {
-      i==0? word = "$word${finalreverse[i]}":word = "$word ${finalreverse[i]}";
-    }
-    return word;
+    return parts.join(' ');
   }
-
   print(reverseWords(testText));
 }
-
-
-
-//I get some trouble in this function I arrived to the reverse  and joining...
-
 /*
-we need to reverse each word in string given , the words not all String ,and all
-spaces should be retained
-So what I must to do?
-1- firstly i need to add every word in the String to a List of Strings
-2- then I need to revers each word 
-3- then I need to made a loop of the String for checking words
-  if a word in the string is == to the reversed word , I will replace it with
-  the reverssed word
-  else null
-  the loop is X²
 
 */

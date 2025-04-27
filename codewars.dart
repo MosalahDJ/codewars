44e6f67585ba442d19e435cf02393e04e6a1bce2
mohamedsalah <djehelmohamedsalah@gmail.com>
2025-04-27 10:58:05 +0100

    add plan

--- codewars.dart
+++ codewars.dart
@@ -1,16 +1,26 @@
 void main() {
-  String testText = "hello  hello";
-String reverseWords(String text) {
-  List<String> parts = text.split(' ');
-    for (int i = 0; i < parts.length; i++) {
-      if (parts[i].isNotEmpty) {
-        parts[i] = parts[i].split('').reversed.join();
+  int num = 12;
+  List<int> divisorsList = [];
+  Object divisors(int number) {
+    for (int i = 0; i < number && number > 1; i--) {
+      if (number % i == 0) {
+        divisorsList.add(i);
       }
     }
-    return parts.join(' ');
+    return [];
   }
-  print(reverseWords(testText));
+
+  print(divisors(num));
 }
+
+
+
+
 /*
+first thing I must do is to make a loop on the numbers under num
+then I must add a list of int 
+then I must add a condition if the number is divisor or not
+if it's divisor add it to the list of int else null
+then I need to an other check
 
 */

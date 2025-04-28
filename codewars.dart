e024017fe900e6ef23f94c2d07ec1e6e9da5cbe3
mohamedsalah <djehelmohamedsalah@gmail.com>
2025-04-28 10:41:09 +0100

    kata completed

--- codewars.dart
+++ codewars.dart
@@ -1,9 +1,20 @@
 void main() {
-  List<List<int>> nums = [[9, 7, 5, 3, 1], [8, 6, 4, 2, 0], [], [1]];
+  List<List<int>> nums = [
+    [-9, 7, 5, 3, 1],
+    [8, 6, 4, 2, 0],
+    [],
+    [1],
+  ];
 
-  List<int> flattenAndSort(List<List<int>> nums) {
-  return [];
-}
+  List<int> flattenAndSort(List<List<int>> nums) 
+  {
+    List<int> result = [];
+    for (int i = 0; i < nums.length; i++) {
+      result.addAll(nums[i]);
+      result.sort();
+    }
+    return result;
+  }
 
   print(flattenAndSort(nums));
 }
@@ -12,5 +23,10 @@ void main() {
 
 
 /*
+add a list<int> 
+make a loop of nums 
+then make another loop of every list inside nums x
+add every list to the new list
+then remove the space field
 
 */

88f7095c1f4a2854678034cfb5b887633ff6e154
mohamedsalah <djehelmohamedsalah@gmail.com>
2025-05-10 11:17:29 +0100

    fix: correct factorial calculation logic and initialize result variable

--- codewars.dart
+++ codewars.dart
@@ -2,12 +2,12 @@ void main() {
   int num = 2;
 
   int factorial(int n) {
-    late int res;
+    int res =1;
     if (n == 0) {
       return 1;
     } else {
       for (int i = n; i <= n && i > 0; i--) {
-        res = n * n - 1;
+        res = res * n - 1;
       }
       return res;
     }
